bacf3e639d16afc4fac6d7a14625ba8b4c65187f
ColdPark <ekzm103@gmail.com>
2024-02-11 22:51:00 +0900

    타겟 원 구현
    1, TargetCircle 스크립트 생성후 Rotate 메소드로 회전 구현

---
+++ Assets/Script/TargetCircle.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetCircle : MonoBehaviour
+{
+    [SerializeField]
+    float rotateSpeed = -30f; // 시계방향 음수 , 반시계 방향 양수
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotate();
+    }
+
+    void Rotate()
+    {
+        transform.Rotate(0,0, rotateSpeed);
+    }
+}
